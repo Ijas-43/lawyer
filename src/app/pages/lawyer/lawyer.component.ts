52305b96738bc298fbc81e660a9d7a4638aa0044
IJAZ KHAN. S <ijaskhan4343@gmail.com>
2022-12-22 23:58:02 +0530

    commit

--- src/app/pages/lawyer/lawyer.component.ts
+++ src/app/pages/lawyer/lawyer.component.ts
@@ -12,4 +12,66 @@ export class LawyerComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  details=[
+    {
+      name : "Aslam",
+      image: "./assets/service/avtar.png",
+      number:"+91 9867543456",
+      language:"Tamil, English, Malayalam",
+      practice:"Civil, Criminal, Corporate",
+      mail:" demo3@gmail.com",
+      place:"Kumbakonam, TN",
+      experience:"5Years"
+
+    },
+
+    {
+      name : "Bhuvanesh",
+      image: "./assets/service/avtar.png",
+      number:"+91 9087654321",
+      language:"Tamil, English, Hindi",
+      practice:"Civil, Criminal, Bussiness",
+      mail:" demo3@gmai2.com",
+      place:" Karur, TN",
+      experience:"4Years"
+
+    },
+    {
+      name : "Charles",
+      image: "./assets/service/avtar.png",
+      number:"+91 9867543456",
+      language:"Tamil, English, Malayalam",
+      practice:"Civil, Criminal, Corporate",
+      mail:" demo3@gmail.com",
+      place:"Chennai, TN",
+      experience:"3Years"
+
+    },
+
+    {
+      name : "Dharmadurai",
+      image: "./assets/service/avtar.png",
+      number:"+91 956787343",
+      language:"Tamil, English, Telugu",
+      practice:"Family, Criminal, Corporate",
+      mail:" demo4@gmai3.com",
+      place:"Trichy, TN",
+      experience:"9Years"
+
+    },
+
+    {
+      name : "Eliyas",
+      image: "./assets/service/avtar.png",
+      number:"+91 9866544321",
+      language:"Tamil, English, Malayalam",
+      practice:"Bussiness, Criminal, Corporate",
+      mail:" demo5@gmail.com",
+      place:"Pudukkottai, TN",
+      experience:"7Years"
+
+    },
+
+   
+  ]
 }
